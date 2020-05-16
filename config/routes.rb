a5a46f8cdf6498440ee4fb9021ec33544b4a8a0f
rorono <kosuke19952000@gmail.com>
2020-05-16 13:22:11 +0900

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-
-  devise_for :users
-  root "posts#index"
+  root "home#show"
   resources :posts
   get 'settings/account'
   post 'settings/account'
   get 'settings/password'
   post 'settings/password'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # resource :settings, only: [:edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
