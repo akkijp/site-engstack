4300a9691a58048c5affba35a4b829c57d6803f5
rorono <kosuke19952000@gmail.com>
2020-05-17 12:39:41 +0900

    Update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   skip_before_action :require_login, only: [:show]
   def show
+    @user = User.new
     redirect_to posts_path if logged_in?
   end
 end
