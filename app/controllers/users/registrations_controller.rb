c263fe27d7eb5c4b019a63330a0e672cf09c4b1b
rorono <kosuke19952000@gmail.com>
2020-05-17 13:25:09 +0900

    Update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,11 +7,15 @@ class Users::RegistrationsController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      auto_login(@user)
-      redirect_to root_path, notice: 'User was successfully created'
-    else
-      render :new
+    respond_to do |format|
+      if @user.save
+          auto_login(@user)
+          format.html { redirect_to root_path, notice: 'User was successfully created.' }
+          format.json { render :show, status: :created, location: @user }
+      else
+          format.html { render :new }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
